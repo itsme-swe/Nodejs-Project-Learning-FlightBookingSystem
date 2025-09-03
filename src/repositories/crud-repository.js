3ccf26d96f3a93b1cfd89cf1187c02bb17fc11e7
itsharsh_swe <harsh_s.dev@outlook.com>
2025-09-03 08:30:01 +0530

    Database integration using sequelize

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,73 @@
+const { where } = require("sequelize");
+const { Logger } = require("../config");
+
+class Crudrepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+      const response = await this.model.create(data);
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in Crud Repo: Create");
+      throw error;
+    }
+  }
+
+  async destroy(data) {
+    try {
+      const response = await this.model.destroy({
+        where: {
+          id: data,
+        },
+      });
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in Crud Repo: destroy");
+      throw error;
+    }
+  }
+
+  async get(data) {
+    try {
+      const response = await this.model.findbyPk(data);
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in Crud Repo: get");
+      throw error;
+    }
+  }
+
+  async getAll() {
+    try {
+      const response = await this.model.findAll();
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in Crud Repo: getAll");
+      throw error;
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const response = await this.model.update(data, {
+        where: {
+          id: id,
+        },
+      });
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in Crud Repo: getAll");
+      throw error;
+    }
+  }
+}
+
+module.exports = Crudrepository;
+
+/*
+🔶 Explaination: 
+This pattern is known as the Repository Pattern, and its main purpose is to create a single, reusable place to organize data access logic. This way, actual business logic is kept separate, making the rest of the application cleaner and easier to maintain. Instead of repeating CRUD logic everywhere, every model in the application can use this base class.
+*/
